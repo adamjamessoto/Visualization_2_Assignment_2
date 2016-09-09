0e261a7fd43ccf96639bb189220bab2614819cce
adamjamessoto <adamjamessoto@utexas.edu>
2016-09-08 23:45:40 -0500

    Initial Commit

---
+++ Assignment_2_Ascii_Art/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment_2_Ascii_Art
+//
+//  Created by Adam Soto on 9/8/16.
+//  Copyright © 2016 Adam Soto. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
